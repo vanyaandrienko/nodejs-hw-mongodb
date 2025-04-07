593a641c4041285ad93e321f780e94bb2bb83b19
vanyaandrienko <vanyaandrienko@gmail.com>
2025-04-07 12:35:20 +0300

    aad crud

---
+++ src/db/middlewares/notFoundHandler.js
@@ -0,0 +1,11 @@
+import { HttpError } from 'http-errors';
+
+export const notFoundHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    res.status(404).json({
+      status: 404,
+      message: "Route not found"
+    });
+    return;
+  }
+  };
