e12395d613b481b57a94954c3b0a81b6cd091893
vanyaandrienko <vanyaandrienko@gmail.com>
2025-04-14 19:00:33 +0300

    add validation

---
+++ src/validation/contacts.js
@@ -0,0 +1,18 @@
+import Joi from 'joi';
+import { contactType } from '../constants/contact.js';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().min(3).max(20),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid(...contactType),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().min(3).max(20),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid(...contactType),
+});
