c207a432b4da47a03ad56fcb95da59626cd04cd3
vanyaandrienko <vanyaandrienko@gmail.com>
2025-04-26 19:41:26 +0300

    add

--- src/server.js
+++ src/server.js
@@ -5,41 +5,40 @@ import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import router from './routers/contacts.js';
+import contactsRouter from './routers/contacts.js';
 import authRouter from "./routers/auth.js";
 
-export function setupServer() {      
+export function setupServer() {
+  const PORT = Number(getEnvVar('PORT', '3000'));
 
-    const PORT = Number(getEnvVar('PORT', '3000'));
+  const app = express();
 
-    const app = express();
+  app.use(express.json());
 
-    app.use(express.json());
-    
-    app.use(
-        pino({
-          transport: {
-            target: 'pino-pretty',
-          },
-        }),
-      );
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-    app.use(cors());
+  app.use(cors());
 
-    app.get('/', (req, res) => {
-      res.json({
-        message: 'Hello World!',
-      });
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World!',
     });
-    
-    app.use(router); 
-    app.use("/auth", authRouter);
-   
-    app.use('*', notFoundHandler);
+  });
 
-    app.use(errorHandler);
-  
-    app.listen(PORT, () => {
+  app.use("/api/contacts", contactsRouter);
+  app.use("/api/auth", authRouter);
+
+  app.use('*', notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    });
-};
+  });
+}
