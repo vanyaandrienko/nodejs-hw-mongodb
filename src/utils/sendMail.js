322df68aedabc8fb657ab3a7fb86eca7e9af5d52
vanyaandrienko <vanyaandrienko@gmail.com>
2025-05-01 15:17:23 +0300

    fix

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -6,9 +6,13 @@ import { getEnvVar } from "./getEnvVar.js";
 const transporter = nodemailer.createTransport({
     host: getEnvVar(SMTP.SMTP_HOST),
     port: Number(getEnvVar(SMTP.SMTP_PORT)),
+    secure: false, 
     auth: {
         user: getEnvVar(SMTP.SMTP_USER),
         pass: getEnvVar(SMTP.SMTP_PASSWORD),
+    },
+    tls: {
+        rejectUnauthorized: false, 
     },
 });
 
